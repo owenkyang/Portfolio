e3ed87fe690fc912f8db33d2e5e90058fb17e16e
Owen Yang <owentheyanger@gmail.com>
2024-07-25 09:58:23 -0700

    added particles

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,28 @@
 <script>
   import "../app.css";
-</script>
+  import Footer from "../components/Footer.svelte";
+  import Header from "../components/Header.svelte";
+  let y;
+  let innerHeight = 0
+  let innerWidth = 0
 
-<slot />
+  const goTop = () =>{
+    document.body.scrollIntoView()
+  }
+    
+  
+</script>
+<div class = "relative flex flex-col max-w-[1400px] mx-auto w-full text-sm
+sm:text-base min-h-screen">
+    <div class={"fixed bottom-0 w-full duration-200 flex p-10 z-[10]" + (
+        y > 0 ? ' opacity-full pointer-events-auto' : ' pointer-events-none opacity-0 '
+    )}>
+        <button>
+            <i class = "fa-solid fa-arrow-up"/>
+        </button>
+    </div>
+    <Header y = {y}/>
+    <slot/>
+    <Footer/>
+</div>
+<svelte:window bind:scrollY={y} bind:innerHeight bind:innerWidth />
