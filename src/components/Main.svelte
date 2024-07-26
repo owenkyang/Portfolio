175c2fd6dc7a85270f5f423cc0e71c5395729ae3
Owen Yang <owentheyanger@gmail.com>
2024-07-26 15:33:54 -0700

    small fixes

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -31,7 +31,6 @@
                 <span class="poppins text-violet-400"> <br/> @ UC San Diego</span> 
             </h2>
             <p class="text-base sm:text-lg md:text-xl">
-                Animation to do
             </p>
             <a class="blueShadow mx-auto lg:mr-auto lg:ml-0 text-base sm:text-lg md:text-xl poppins
             relative overflow-hidden px-6 py-3 group rounded-full bg-white text-slate-950 cursor-pointer" href="#"
