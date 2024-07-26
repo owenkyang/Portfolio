dba76195204660b3a5b5132112fad3ea5ee4f67d
Owen Yang <owentheyanger@gmail.com>
2024-07-25 22:57:03 -0700

    added projects section

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -1,10 +1,15 @@
 <script>
-
+    import Step from "./Step.svelte"
+    let steps = [
+        {name: 'AI Expense Tracker', icon: "fa-solid fa-coins"},
+        {name: 'Football Analyzer', icon: "fa-solid fa-futbol"},
+        {name: 'Coming soon', icon: "fa-solid fa-code"}
+    ]
 </script>
 
 <main class = "flex flex-col flex-1 p-4">
-    <section id= "introPage" class = "grid grid-cols-1 lg:gird-cols-2 gap-10 py-8 sm:py-14">
-        <div class = "flex flex-col lg:justify-center text-center lg:text-left gap6 md:gap-8 lg:gap-10">
+    <section id= "introPage" class = "grid grid-cols-1 lg:grid-cols-2 gap-10 py-8 sm:py-14">
+        <div class = "flex flex-col lg:justify-center text-center lg:text-left gap-6 md:gap-8 lg:gap-10">
             <h2 class = "font semibold text-4xl sm:text-5xl md:text-6xl">
                 Hi! I'm <span class = "poppins text-violet-400">Owen</span> Yang <br/> Data Science Student
                 <span class = "poppins text-violet-400"> <br/> @ UC San Diego</span> 
@@ -12,7 +17,7 @@
             <p class = "text-base sm:text-kg md:text-xl">
                 Animation to do
             </p>
-            <a class = "blueShadow mx-auto lg:mr-auto text-base sm:text-lg md:text-xl poppins
+            <a class = "blueShadow mx-auto lg:mr-auto lg:ml-0 text-base sm:text-lg md:text-xl poppins
             relative overflow-hidden px-6 py-3 group rounded-full bg-white text-slate-950 cursor-pointer" href = "#"
             target = "_blank">
             <div
@@ -23,7 +28,36 @@
         </a>
         </div>
         <div class = "relative shadow-2xl grid place-items-center">
-            <img src={"images/profile.png"} alt="Profile Image" class="object-cover z-[2] max-h-70[vh]"/>
+            <img src={"images/profile.png"} alt="Profile Image" class="object-cover z-[2] max-h-70[vh]" width = 535/>
         </div>
+    </section>
+    <section id = "projects" class = "py-20 lg:py-32 flex flex-col gap-24">
+        <div class = "flex flex-col gap-2 text-center">
+            <h6 class = "text-lg sm:text-xl md:text-2xl">
+                A few of my endeavors.
+            </h6>
+            <h3 class = "font semibold text-3xl sm:text-4xl md:text-5xl">
+                Curious to <span class = "poppins text-violet-400">indulge</span> in my work?
+            </h3>
+        </div>
+        <a href="https://github.com/owenkyang"
+            target ="_blank" class="mx-auto px-10 py-2 rounded-md border border-solid border-white flex items-center gap-2 -mb-4 sm:-mb-0
+            -mt-10 hover:border-violet-700 duration-200">
+            <i class="fa-brands fa-github"/>
+            <p>GitHub</p>
+        </a>
+        <div class="grid grid-cols-1 lg:grid-cols-3 gap-12 lg:gap-10">
+            <Step step={steps[0]}>
+            <p>Keep track of your investments, expenses, and savings whilst getting financial advice from an AI assistant!</p>
+            <!-- Add tech stack logos -->
+            </Step>
+            <Step step={steps[1]}>
+                
+            </Step>
+            <Step step={steps[2]}>
+                
+            </Step>
+        </div>
+
     </section>
 </main>
